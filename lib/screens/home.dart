59eab2b58b79c4e6a6bb732ffa02929ff17a87a6
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-06-26 17:45:01 +0100

    Firstcommit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +7,118 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _bodyUI(context),
+    );
+  }
+
+  Widget _bodyUI(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.height * 0.02,
+          vertical: MediaQuery.of(context).size.width * 0.01,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _topBar(context),
+            _title(),
+            _searchField(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _topBar(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.06,
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              "https://i.pravatar.cc/150?img=62",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _title() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Discover',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+        Text(
+          'Get the best sneakers here',
+          style: TextStyle(
+            color: Colors.black26,
+            fontWeight: FontWeight.w500,
+            fontSize: 15,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _searchField(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.06,
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.04),
+      margin: EdgeInsets.symmetric(
+        vertical: MediaQuery.of(context).size.height * 0.02,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 1,
+            blurRadius: 10,
+          ),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.search,
+            color: Colors.black26,
+          ),
+          SizedBox(
+            width: MediaQuery.sizeOf(context).width * 0.70,
+            child: const TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search your favorite',
+                hintStyle: TextStyle(
+                  color: Color.fromRGBO(115, 115, 115, 1),
+                  fontSize: 15,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
